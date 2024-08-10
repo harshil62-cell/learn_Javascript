7d47154af9ef3bcf45c1df1c84b7d2194cb583e1
harshil62-cell <151919170+harshil62-cell@users.noreply.github.com>
2024-08-10 10:00:41 +0000

    commit changes done

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -14,3 +14,16 @@ console.log(b1)
 //similarly when we conver boolean true to number it will show 1 and false will show 0
 console.log(typeof NaN)//type of NaN is number
 //1=>true 0=>false ""=>false "harshil"=>true from datatype to boolean
+//****************operations******************* */
+let str1="Harshil"
+let str2=" Vasani"
+let str3=str1+str2//concatenation
+console.log(str3)
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+"2")//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+//this is done according to ecma guidelines tc39
+console.log(+true)//1
+console.log(+"")//0
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,6 @@
+console.log("2">1)
+console.log(null>0)//comparison
+console.log(null==0)//==equality check
+console.log(null>=0)//comparison converts null to a number
+//we should avoid such conversions
+//=== not only checks value but also its data type
