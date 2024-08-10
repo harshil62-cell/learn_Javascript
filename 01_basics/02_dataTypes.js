1f2212dae4c48b242021de331ba3e1ae96cda053
harshil62-cell <151919170+harshil62-cell@users.noreply.github.com>
2024-08-10 08:46:53 +0000

    commit changes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all JS code as newer version
+//types of data in JS
+//number
+//string
+//boolean
+//null->standalone value /representation of empty value
+//undefined->when value is not defined yet 
+//symbol
+
+//object
+console.log(typeof "harshil")
+console.log(typeof null)//will return that type of null is object
