bd53cedc86da5d120a6fc61299837096df224174
harshil62-cell <151919170+harshil62-cell@users.noreply.github.com>
2024-08-10 08:29:58 +0000

    commit changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=12367
+let accountName="Harshil"
+var accountMail="Harshil@gmail.com"
+accountCity="Valsad"//avoid declaring variable like this
+let accountState //undefined as not initialised
+console.table([accountId,accountMail,accountName,accountCity,accountState])
